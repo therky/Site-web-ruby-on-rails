772c33f7f85fd8ae16c2e524686793c425cd65dd
Kassim Therrien <kassim_therrien@hotmail.ca>
2017-06-12 00:06:51 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,6 @@ class ProfilesController < ApplicationController
   # GET to /users/:users_id/profile/new
   def new
     # Render blank profile details form
-    
+    @profile = Profile.new
   end
 end 
